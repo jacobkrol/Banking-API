f9c872f00e3a3d7e3277a7e6e67d59ac844394a9
Jacob Krol <jkrol5@uic.edu>
2019-12-09 19:20:54 +0000

    Update root with documentation link

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,5 @@ const router = express.Router();
 module.exports = router;
 
 router.get('/', (req, res) => {
-   res.send('Hello World!');
+   res.send('Welcome to the root directory for the IT 302 Banking API. For documentation, visit the <a href="https://github.com/jacobkrol/Banking-API/blob/master/README.md" target="_blank">GitHub page</a>.');
 });
